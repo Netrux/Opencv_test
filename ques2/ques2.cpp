c55281619d23aa48c3c1c17607e7fcd2c6325467
netrux <netruxs07@gmail.com>
2021-05-19 20:48:26 +0530

    ques 2 completed using normal method

--- ques2/ques2.cpp
+++ ques2/ques2.cpp
@@ -21,11 +21,33 @@ int main(int argc, char** argv)
     }
 
 	threshold(src,dst, 20, 200, THRESH_BINARY);
+	int flag = 0;
+	Point pt1,pt2;
+	for(int i = 0; i < dst.cols - 100; i++){
 
-	
-	
-    cv::imshow("input image ",dst);
-   // cv::imshow("cropped image ",image2);
+		Vec3f intensity = dst.at<Vec3f>(i, 1475);
+            	float blue = intensity.val[0];
+		if(blue == 0 && flag == 0){
+			cout<<"pixel value" <<i<<endl;
+			flag = 1;
+			pt1.x = 1500;
+			pt1.y = i;
+			pt2.x = 0;
+			pt2.y = i;
+			line( src, pt1, pt2, Scalar(0,0,255), 1, LINE_AA);
+			}
+		else if(blue != 0 && flag ==1){
+			cout<<"pixel value"<<i<<endl;
+			flag = 0;
+			pt1.x = 1500;
+			pt1.y = i;
+			pt2.x = 0;
+			pt2.y = i;
+			line( src, pt1, pt2, Scalar(0,0,255), 1, LINE_AA);
+			}
+}
+    cv::imshow("input image ",src);
+	imwrite("result.png", src);
     cv::waitKey(0);
 
     return(0);
